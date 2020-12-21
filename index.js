23974f603b2bd7bde0b08ee06837f41db3a4a72b
Nishan <nishanbende@gmail.com>
2020-12-21 10:46:45 +0530

    only activate in dev env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-export const blurElementStyle = {
+const IS_DEV = process.env.NODE_ENV === "development";
+
+export const blurElementStyle = IS_DEV ? {
   '-webkit-filter': 'blur(10px)',
   '-moz-filter': 'blur(10px)',
   '-o-filter': 'blur(10px)',
@@ -9,4 +11,4 @@ export const blurElementStyle = {
   'flexDirection': 'column',
   'pointerEvents': 'none',
   'cursor': 'default',
-};
+}: {};
