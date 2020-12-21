f65094a765b8379d198472a5d927fdd834997a00
Nishan <nishanbende@gmail.com>
2020-12-21 11:33:13 +0530

    remove display property

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const IS_DEV = process.env.NODE_ENV === "development";
+const IS_DEV = process.env.NODE_ENV === 'development';
 
-export const blurElementStyle = IS_DEV ? {
-  '-webkit-filter': 'blur(10px)',
-  '-moz-filter': 'blur(10px)',
-  '-o-filter': 'blur(10px)',
-  '-ms-filter': 'blur(10px)',
-  'filter': 'blur(10px)',
-  'height': '100%',
-  'display': 'flex',
-  'flexDirection': 'column',
-  'pointerEvents': 'none',
-  'cursor': 'default',
-}: {};
+export const blurElementStyle = IS_DEV
+  ? {
+      '-webkit-filter': 'blur(10px)',
+      '-moz-filter': 'blur(10px)',
+      '-o-filter': 'blur(10px)',
+      '-ms-filter': 'blur(10px)',
+      'filter': 'blur(10px)',
+      'pointerEvents': 'none',
+      'cursor': 'default',
+    }
+  : {};
